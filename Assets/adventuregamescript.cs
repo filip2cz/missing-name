ded49cbf99d3dc0f420e4fe5c7549926afe8938d
filip2cz <filip@fkomarek.eu>
2024-02-04 00:37:49 +0100

    I FOUND SOURCE CODE part1

---
+++ Assets/adventuregamescript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class adventuregamescript : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State stateNow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stateNow = startingState;
+        textComponent.text = stateNow.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateState();
+    }
+
+    private void updateState()
+    {
+        var nextStates = stateNow.GetNextStates();
+        for (int index = 0; index < nextStates.Length; index ++)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad1 + index))
+            {
+                stateNow = nextStates[index];
+            }
+        }
+        textComponent.text = stateNow.GetStateStory();
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # missing-name
 My first and propably last unity game. Created 1/06/2020.
-Source code is propably lost, but I have Unity web engine build.
 You can play it here: http://fkomarek.eu/games/1/index.html
